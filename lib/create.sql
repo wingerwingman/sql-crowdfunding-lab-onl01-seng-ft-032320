ec3655d0f7b845c42dd984617b28c9f1d4ebd249
Justin Winger <justinwinger4@gmail.com>
2020-04-20 16:19:03 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,8 +3,8 @@ CREATE TABLE projects (
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date INTEGER,
-  end_date INTEGER
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
