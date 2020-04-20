36c4bd9f918b97aab8c67b968f3b9af0f2ee2a2e
Justin Winger <justinwinger4@gmail.com>
2020-04-20 16:14:35 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,16 @@ CREATE TABLE projects (
   start_date INTEGER,
   end_date INTEGER
 );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+)
