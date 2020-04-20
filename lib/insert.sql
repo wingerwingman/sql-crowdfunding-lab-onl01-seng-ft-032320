5913500254717d6735ea0acbf96921879d7a0919
Justin Winger <justinwinger4@gmail.com>
2020-04-20 16:29:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,16 @@
+# users
+INSERT INTO users VALUES (1,"Doug", 40);
+INSERT INTO projects VALUES (2, "Adam", "34");
+INSERT INTO projects VALUES (3, "sam", 21);
+INSERT INTO projects VALUES (4, "Jasmin", 16);
+INSERT INTO projects VALUES (5, "Cat", 67);
+INSERT INTO projects VALUES (6, "James", 24);
+INSERT INTO projects VALUES (7, "Chris", 18);
+INSERT INTO projects VALUES (8, "Sam", 35);
+INSERT INTO projects VALUES (9, "Dan", 34);
+INSERT INTO projects VALUES (10,"Tracy", 23);
+
+# projects
 INSERT INTO projects VALUES (1,"Moon", "landing", 500, "12/21/1999", "1/15/1920");
 INSERT INTO projects VALUES (2, "Dog", "Peting", 200,000, "1/21/1999", "1/31/1920");
 INSERT INTO projects VALUES (3, "Carpet Ride", "Traveling", "50", "12/14/1999", "6/15/1920");
