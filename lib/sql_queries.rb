a566ad3f237464740451cce442e7f4b40afacfe4
Justin Winger <justinwinger4@gmail.com>
2020-04-20 17:26:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal  FROM projects
+    INNER JOIN pledges
+      ON projects.id = pledges.project_id
+    GROUP BY projects.title
+    HAVING over_goal >= 0;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
