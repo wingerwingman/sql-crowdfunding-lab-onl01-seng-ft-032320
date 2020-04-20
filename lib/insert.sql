33bbda345d2a068f59cde5c151a231763f8394a0
Justin Winger <justinwinger4@gmail.com>
2020-04-20 16:30:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,24 +1,24 @@
 # users
 INSERT INTO users VALUES (1,"Doug", 40);
-INSERT INTO projects VALUES (2, "Adam", "34");
-INSERT INTO projects VALUES (3, "sam", 21);
-INSERT INTO projects VALUES (4, "Jasmin", 16);
-INSERT INTO projects VALUES (5, "Cat", 67);
-INSERT INTO projects VALUES (6, "James", 24);
-INSERT INTO projects VALUES (7, "Chris", 18);
-INSERT INTO projects VALUES (8, "Sam", 35);
-INSERT INTO projects VALUES (9, "Dan", 34);
-INSERT INTO projects VALUES (10,"Tracy", 23);
+INSERT INTO users VALUES (2, "Adam", "34");
+INSERT INTO users VALUES (3, "sam", 21);
+INSERT INTO users VALUES (4, "Jasmin", 16);
+INSERT INTO users VALUES (5, "Cat", 67);
+INSERT INTO users VALUES (6, "James", 24);
+INSERT INTO users VALUES (7, "Chris", 18);
+INSERT INTO users VALUES (8, "Sam", 35);
+INSERT INTO users VALUES (9, "Dan", 34);
+INSERT INTO users VALUES (10,"Tracy", 23);
 INSERT INTO users VALUES (11,"Doug", 40);
-INSERT INTO projects VALUES (12, "Adam", "34");
-INSERT INTO projects VALUES (13, "sam", 21);
-INSERT INTO projects VALUES (14, "Jasmin", 16);
-INSERT INTO projects VALUES (15, "Cat", 67);
-INSERT INTO projects VALUES (16, "James", 24);
-INSERT INTO projects VALUES (17, "Chris", 18);
-INSERT INTO projects VALUES (18, "Sam", 35);
-INSERT INTO projects VALUES (19, "Dan", 34);
-INSERT INTO projects VALUES (20,"Tracy", 23);
+INSERT INTO users VALUES (12, "Adam", "34");
+INSERT INTO users VALUES (13, "sam", 21);
+INSERT INTO users VALUES (14, "Jasmin", 16);
+INSERT INTO users VALUES (15, "Cat", 67);
+INSERT INTO users VALUES (16, "James", 24);
+INSERT INTO users VALUES (17, "Chris", 18);
+INSERT INTO users VALUES (18, "Sam", 35);
+INSERT INTO users VALUES (19, "Dan", 34);
+INSERT INTO users VALUES (20,"Tracy", 23);
 
 # projects
 INSERT INTO projects VALUES (1,"Moon", "landing", 500, "12/21/1999", "1/15/1920");
