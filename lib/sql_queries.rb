a7d9a5ece4bb94bfbaec620e472493787a9f4457
Justin Winger <justinwinger4@gmail.com>
2020-04-20 17:32:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT projects.category, pledges.amoun FROM projects
+  "SELECT projects.category, pledges.amount FROM projects
     INNER JOIN pledges
       ON projects.id = pledges.project_id
     WHERE category = 'music';"
