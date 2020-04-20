531a28436712542adc12116d93ec5c6b10b415c3
Justin Winger <justinwinger4@gmail.com>
2020-04-20 17:20:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,10 +21,10 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) FROM users
+  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal  FROM projects
     INNER JOIN pledges
-      ON users.id = pledges.user_id
-    GROUP BY users.name;"
+      GROUP BY projects.title
+    HAVING over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
