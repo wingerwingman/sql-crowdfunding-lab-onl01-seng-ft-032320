0aa4d0a4e0b8f29b23eda8c2f4a133c11848daf0
Justin Winger <justinwinger4@gmail.com>
2020-04-20 16:34:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,16 @@
 
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", 500, "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "Peting", 200,000, "1/21/1999", "1/31/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Carpet Ride", "Traveling", "50", "12/14/1999", "6/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
+
 INSERT INTO users VALUES (1,"Doug", 40);
 INSERT INTO users VALUES (2, "Adam", "34");
 INSERT INTO users VALUES (3, "sam", 21);
@@ -19,15 +31,3 @@ INSERT INTO users VALUES (17, "Chris", 18);
 INSERT INTO users VALUES (18, "Sam", 35);
 INSERT INTO users VALUES (19, "Dan", 34);
 INSERT INTO users VALUES (20,"Tracy", 23);
-
-
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", 500, "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog", "Peting", 200,000, "1/21/1999", "1/31/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Carpet Ride", "Traveling", "50", "12/14/1999", "6/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Moon", "landing", "500", "12/21/1999", "1/15/1920");
