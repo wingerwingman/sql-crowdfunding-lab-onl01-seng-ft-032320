801d75bce59ea44b1c57af0ef86a27069520282c
Justin Winger <justinwinger4@gmail.com>
2020-04-20 16:46:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount) FROM projects 
     INNER JOIN pledges
-      ON project.id = pledges.project_id 
+      ON projects.id = pledges.project_id 
     GROUP BY projects.title;"
 end
 
